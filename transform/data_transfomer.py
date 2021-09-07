75c06df0c29d5ad91dab7d3728314299ba542869
syhcmus <phamminhsyhcmus@gmail.com>
2021-09-07 22:10:40 +0700

    add module for transforming data

---
+++ transform/data_transfomer.py
@@ -0,0 +1,43 @@
+
+
+from os import name
+
+
+GEN_MAP = {'A': '1', 'C': '2', 'G': '3', 'T': '4' }
+
+def transform(input_file, out_file = 'data/transformed_data.txt'):
+    with open(input_file, 'r') as input:
+        new_lines = []
+        lines = input.readlines()
+        for line in lines:
+            gen_list = []
+            for i in range(len(line.strip())):
+                key = line[i]
+                gen_list.append(GEN_MAP.get(key))
+
+        
+            new_line = ' -1 '.join(gen_list)
+            new_line += ' -2'
+           
+
+            new_lines.append(new_line)
+
+        new_lines = '\n'.join(new_lines)
+
+
+        with open(out_file, 'w') as output:
+            output.write(new_lines)
+
+
+if __name__ == '__main__':            
+
+    input_file = "data/MT291827.txt"
+
+    transform(input_file=input_file)
+
+    print("Data transformed !!!")
+
+
+
+
+    
