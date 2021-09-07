651739000d5a8e5618c0ef2ac6e5927221db4208
syhcmus <phamminhsyhcmus@gmail.com>
2021-09-07 22:10:54 +0700

    add module for transforming data

--- transform/test.py
+++
@@ -1,7 +0,0 @@
-f = open("demofile3.txt", "a")
-f.writelines(["\nSee you soon!", "\nOver and out."])
-f.close()
-
-#open and read the file after the appending:
-f = open("demofile3.txt", "r")
-print(f.read())
