439d971cded77814602688415724e38432d483b1
sypm <sypm@minerva.vn>
2022-02-16 16:41:33 +0700

    update model for feature selective

--- virulence_classification/feature_selective/model.py
+++ virulence_classification/feature_selective/model.py
@@ -1,4 +1,3 @@
-from sklearn import pipeline
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -6,14 +5,12 @@ from sklearn.feature_selection import SelectFromModel
 from lightgbm import LGBMClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-
-import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle as pkl
 
 
 
-def score(test_y, pred_y, prob_y):
+def score(test_y, pred_y):
     '''
     Score the prediction
     Input: true label, predicted label, probility of predicted label
@@ -48,7 +45,7 @@ def train(train_x, train_y):
 
     steps = [('SFM', SelectFromModel(estimator=extraTree)),
              ('Scaler', StandardScaler()),
-             ('CLF', clf)]
+             ('CLF', model)]
 
     pipeline = Pipeline(steps)
 
@@ -73,21 +70,6 @@ def predict(test_x, test_y):
 
     pred_y = pipeline.predict(test_x)
     prob_y = pipeline.predict_proba(test_x)
-    acc, pre, re, f1 = score(test_y, pred_y, prob_y)
+    acc, pre, re, f1 = score(test_y, pred_y)
 
     return acc, pre, re, f1
-
-
-
-    # prob_y = prob_y[:,1]
-    # draw_roc_curve(test_y, prob_y, dir)
-    # prob_y = pd.DataFrame(prob_y, columns=['Probability of 1'])
-    # test_y = pd.DataFrame(test_y.values, columns=['Indicator'])
-    # result = pd.concat([test_y, pred_y, prob_y], axis=1, sort=False)
-    # result.to_csv(dir + 'Prediction.csv', index=False)
-
-    # print('Accuracy: ', acc)
-    # print('Precision: ', pre)
-    # print('Recall: ', re)
-    # print('F1 Score: ', f1)
-    # print('ROC AUC Score', roc)   
