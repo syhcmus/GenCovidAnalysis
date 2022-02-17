aae93563c783a8906fdea767256f28a623b73064
sypm <sypm@minerva.vn>
2022-02-17 16:48:27 +0700

    update occurence calculate

--- virulence_classification/k_mer/calculate_occurence.py
+++ virulence_classification/k_mer/calculate_occurence.py
@@ -1,37 +1,47 @@
 import numpy as np
 import itertools
 
+def get_k_mers(length):
 
+    '''
+    Get the k-mer set with the specific length
+    Input: length of k-mer
+    Output: set of k-mer
+    '''
+    (dict_,col_index) = ({}, 0)
 
-def get_motifs_V2(length):
+    base = ['A','C','G','T']
+    elements =  list(itertools.product(base,repeat = length))
+    elements = [''.join(tups) for tups in elements]
+    for e in elements:
+        dict_[e] = col_index
+        col_index += 1
 
-    (d,index) = ({}, 0)
-    lst = ['A','C','G','T']
-    els =  list(itertools.product(lst,repeat = length))
-    els = [''.join(tups) for tups in els]
-    for elems in els:
-        d[elems] = index
-        index+=1
-
-    return d
+    return dict_
 
 def calculate_occurrences(input_file, length=7):
+    '''
+    Calulate occurence of k-mers set with the specify length
+    Input: data containe sequence, length of k-mer
+    Output: occuence of k-mers set
+    '''
+
     print("calculating occurence ...")
     sequences_list = input_file["Sequence"]
-    d = get_motifs_V2(length)   
+    dict_ = get_k_mers(length)   
     rows_num = len(sequences_list)
-    cols_num = len(d)
-    #with open("k-mers.txt","w") as out_file:
-     #   pprint(d, stream=out_file)
+    cols_num = len(dict_)
 
     data = np.zeros(shape=(rows_num,cols_num))
     y_indicator = np.zeros(shape=(rows_num,1))
+
     for row_idx, seq in enumerate(sequences_list):
         for i in range(0, len(seq)-length+1):
             word = seq[i:i+length]
-            if word in d:
-                col_idx = d[word]
+            if word in dict_:
+                col_idx = dict_[word]
                 data[row_idx, col_idx] += 1
+                
         y_indicator[row_idx] = input_file["Indicator"][row_idx]
 
     return (data,y_indicator)
